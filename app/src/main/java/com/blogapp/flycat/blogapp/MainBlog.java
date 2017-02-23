f878f84bdd2e3b8c0c1317101478fda143cee7e1
Javier Alejandro Lopez Rangel <Javier Alejandro Lopez Rangel>
2017-02-22 20:18:30 -0600

    Compartir proyecto desde android a github

---
+++ app/src/main/java/com/blogapp/flycat/blogapp/MainBlog.java
@@ -0,0 +1,13 @@
+package com.blogapp.flycat.blogapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainBlog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_blog);
+    }
+}
